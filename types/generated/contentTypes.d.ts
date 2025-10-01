006c63b5ac14132f520707e794b5bd256a2e583b
Mustafa Mohamed <mo.mustafa3@gmail.com>
2025-10-01 12:30:02 +0400

    SQ5-126: [FE] Services - Statues in Strapi for all services

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -819,9 +819,19 @@ export interface ApiOrderOrder extends Struct.CollectionTypeSchema {
     package: Schema.Attribute.Relation<'oneToOne', 'api::package.package'>;
     paymentId: Schema.Attribute.String;
     paymentStatus: Schema.Attribute.Enumeration<
-      ['pending', 'paid', 'refunded', 'failed']
+      [
+        'Pending payment',
+        'Payment failed',
+        'Payment confirmed',
+        'Rescheduled',
+        'Sent to vendor',
+        'Cancelled',
+        'QC/Feedback',
+        'Completed',
+        'Refunded',
+      ]
     > &
-      Schema.Attribute.DefaultTo<'pending'>;
+      Schema.Attribute.DefaultTo<'Pending payment'>;
     price: Schema.Attribute.Decimal;
     publishedAt: Schema.Attribute.DateTime;
     responseId: Schema.Attribute.String;
